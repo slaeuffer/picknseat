c4a1cfe8daa934b6c3927d5e728ede51c352f6af
fpalacino <franciscopalacino@gmail.com>
2022-05-05 16:51:49 +0200

    profilepage

--- lib/ordermenu.dart
+++ lib/ordermenu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:picknsteat/profile.dart';
 import 'main.dart';
 import 'products.dart';
 import 'globals.dart';
@@ -34,108 +35,128 @@ class _OrderMenueState extends State<OrderMenue> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: Text('Order'),
-            actions: [
-              Badge(
-                position: BadgePosition.topEnd(top:5, end: 5),
-                badgeColor: Colors.deepPurple,
-                badgeContent: Text((globalvars ?? new Globals()).cart_count.toString(), style: TextStyle(color: Colors.white)),
-                child: IconButton(icon: Icon(Icons.shopping_cart), onPressed: () => {
-                  Navigator.push(context, MaterialPageRoute<void>(
+      appBar: AppBar(title: Text('Order'), actions: [
+        Badge(
+          position: BadgePosition.topEnd(top: 5, end: 5),
+          badgeColor: Colors.deepPurple,
+          badgeContent: Text(
+              (globalvars ?? new Globals()).cart_count.toString(),
+              style: TextStyle(color: Colors.white)),
+          child: IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () => {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute<void>(
                       builder: (BuildContext context) => CartMenu()))
-                },),
-              ),
-              IconButton(icon: Icon(Icons.search), onPressed: () => {},)
-            ]
-
-        ),
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: [
-              DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-                child: CircleAvatar(
-                  radius: 16,
-                  child: ClipRRect(
-                    child: Image.asset("assets/default-avatar.png",)
-                  ),
-                ),
-              ),
-              ListTile(
-                title: const Text('Log off'),
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute<void>(
-                      builder: (BuildContext context) => SignIn()));
-                },
-              ),
-            ],
-          )
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              shopCard(1, "McDonald's"),
-              shopCard(2, "Burger King"),
-              shopCard(1, "McDonald's"),
-              shopCard(2, "Burger King"),
-              shopCard(1, "McDonald's"),
-              shopCard(2, "Burger King"),
-
-            ],
+            },
           ),
         ),
-
+        IconButton(
+          icon: Icon(Icons.search),
+          onPressed: () => {},
+        )
+      ]),
+      drawer: Drawer(
+          child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: CircleAvatar(
+              radius: 16,
+              child: ClipRRect(
+                  child: Image.asset(
+                "assets/default-avatar.png",
+              )),
+            ),
+          ),
+          ListTile(
+            title: const Text('Log off'),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute<void>(
+                      builder: (BuildContext context) => SignIn()));
+            },
+          ),
+          ListTile(
+            title: const Text('My Profile'),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute<void>(
+                      builder: (BuildContext context) => Profile()));
+            },
+          ),
+        ],
+      )),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            shopCard(1, "McDonald's"),
+            shopCard(2, "Burger King"),
+            shopCard(1, "McDonald's"),
+            shopCard(2, "Burger King"),
+            shopCard(1, "McDonald's"),
+            shopCard(2, "Burger King"),
+          ],
+        ),
+      ),
     );
   }
-  
+
   Widget shopCard(int idShop, String txt) {
     return Padding(
-        padding: EdgeInsets.only(left: 10, right: 10, top: 8),
-        child: Card(
-            child: InkWell(
-              onTap: goToShop,
-              child: Row(
-                  children: [
-                    Image.asset("assets/" + idShop.toString() + ".png", scale:1.7),
-                    Padding(
-                        padding: EdgeInsets.only(left: 15),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(txt, style: TextStyle(fontSize: 18.0),),
-                            Padding(
-                                padding: EdgeInsets.only(top: 2),
-                                child: Text("Fast food", style: TextStyle(fontSize: 14.0, color: Colors.grey),),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(top: 3),
-                              child: Row(
-                                children: [
-                                  Icon(Icons.star, size: 14,),
-                                  Text('4.3')
-                                ],
-                              )
-                            )
-                          ],
-                        )
+      padding: EdgeInsets.only(left: 10, right: 10, top: 8),
+      child: Card(
+          child: InkWell(
+        onTap: goToShop,
+        child: Row(children: [
+          Image.asset("assets/" + idShop.toString() + ".png", scale: 1.7),
+          Padding(
+              padding: EdgeInsets.only(left: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    txt,
+                    style: TextStyle(fontSize: 18.0),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 2),
+                    child: Text(
+                      "Fast food",
+                      style: TextStyle(fontSize: 14.0, color: Colors.grey),
                     ),
-                    Padding(
-                        padding: EdgeInsets.only(left: 100),
-                        child: Icon(Icons.arrow_forward_ios)
-                    )
-                  ]
-              ),
-            )
-        ),
+                  ),
+                  Padding(
+                      padding: EdgeInsets.only(top: 3),
+                      child: Row(
+                        children: [
+                          Icon(
+                            Icons.star,
+                            size: 14,
+                          ),
+                          Text('4.3')
+                        ],
+                      ))
+                ],
+              )),
+          Padding(
+              padding: EdgeInsets.only(left: 100),
+              child: Icon(Icons.arrow_forward_ios))
+        ]),
+      )),
     );
   }
 
   void goToShop() {
-    Navigator.pushReplacement(context, MaterialPageRoute<void>(
-        builder: (BuildContext context) => ProductsMenu(globalvars)));
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute<void>(
+            builder: (BuildContext context) => ProductsMenu(globalvars)));
   }
 }
--- lib/profile.dart
+++ lib/profile.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'home_page.dart';
 
-class OrderMenu extends StatelessWidget {
+class ProfileDef extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Picknseat',
+      title: 'Profile Page',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -23,80 +23,171 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
-
+  bool showPassword = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          leading: Icon(Icons.menu),
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          elevation: 1,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back, color: Colors.blue),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
           title: Text('Order'),
           actions: [
-            IconButton(icon: Icon(Icons.shopping_cart), onPressed: () => {},),
-            IconButton(icon: Icon(Icons.search), onPressed: () => {},)
-          ]
-
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            shopCard(1, "McDonald's"),
-            shopCard(2, "Burger King"),
-            shopCard(1, "McDonald's"),
-            shopCard(2, "Burger King"),
-            shopCard(1, "McDonald's"),
-            shopCard(2, "Burger King"),
-
-          ],
+            IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () => {},
+            ),
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () => {},
+            ),
+          ]),
+      body: Container(
+        padding: EdgeInsets.only(left: 16, top: 25, right: 16),
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: ListView(
+            children: [
+              Text(
+                "My Profile",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Center(
+                child: Stack(
+                  children: [
+                    Container(
+                      width: 130,
+                      height: 130,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                            width: 4,
+                            color: Theme.of(context).scaffoldBackgroundColor),
+                        boxShadow: [
+                          BoxShadow(
+                              spreadRadius: 2,
+                              blurRadius: 10,
+                              color: Colors.black.withOpacity(0.1),
+                              offset: Offset(0, 10))
+                        ],
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                              "https://images.pexels.com/photos/3307758/pexels-photo-3307758.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=250"),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            width: 4,
+                            color: Theme.of(context).scaffoldBackgroundColor,
+                          ),
+                          color: Colors.blue,
+                        ),
+                        child: Icon(
+                          Icons.edit,
+                          color: Colors.white,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 35,
+              ),
+              buildTextField("Name", "Jennifer Houston", false),
+              buildTextField("Email", "jef.h@mail.com", false),
+              buildTextField("Password", "jiji", true),
+              buildTextField("Adress", "Garbary 1 st", false),
+              SizedBox(
+                height: 35,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  OutlineButton(
+                    padding: EdgeInsets.symmetric(horizontal: 50),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    onPressed: () {},
+                    child: Text("CANCEL",
+                        style: TextStyle(
+                            fontSize: 14,
+                            letterSpacing: 2.2,
+                            color: Colors.black)),
+                  ),
+                  RaisedButton(
+                    onPressed: () {},
+                    color: Colors.blue,
+                    padding: EdgeInsets.symmetric(horizontal: 50),
+                    elevation: 2,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Text(
+                      "SAVE",
+                      style: TextStyle(
+                          fontSize: 14,
+                          letterSpacing: 2.2,
+                          color: Colors.white),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
-
     );
   }
 
-  Widget shopCard(int idShop, String txt) {
+  Widget buildTextField(
+      String labelText, String placeholder, bool isPasswordTextField) {
     return Padding(
-      padding: EdgeInsets.only(left: 10, right: 10, top: 8),
-      child: Card(
-          child: InkWell(
-            onTap: goToShop,
-            child: Row(
-                children: [
-                  Image.asset("assets/" + idShop.toString() + ".png", scale:1.7),
-                  Padding(
-                      padding: EdgeInsets.only(left: 15),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(txt, style: TextStyle(fontSize: 18.0),),
-                          Padding(
-                            padding: EdgeInsets.only(top: 2),
-                            child: Text("Fast food", style: TextStyle(fontSize: 14.0, color: Colors.grey),),
-                          ),
-                          Padding(
-                              padding: EdgeInsets.only(top: 3),
-                              child: Row(
-                                children: [
-                                  Icon(Icons.star, size: 14,),
-                                  Text('4.3')
-                                ],
-                              )
-                          )
-                        ],
-                      )
-                  ),
-                  Padding(
-                      padding: EdgeInsets.only(left: 100),
-                      child: Icon(Icons.arrow_forward_ios)
+      padding: const EdgeInsets.only(bottom: 35.0),
+      child: TextField(
+        obscureText: isPasswordTextField ? showPassword : false,
+        decoration: InputDecoration(
+            suffixIcon: isPasswordTextField
+                ? IconButton(
+                    onPressed: () {
+                      setState(() {
+                        showPassword = !showPassword;
+                      });
+                    },
+                    icon: Icon(
+                      Icons.remove_red_eye,
+                      color: Colors.grey,
+                    ),
                   )
-                ]
-            ),
-          )
+                : null,
+            contentPadding: EdgeInsets.only(bottom: 3),
+            labelText: labelText,
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+            hintText: placeholder,
+            hintStyle: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            )),
       ),
     );
-  }
-
-  void goToShop() {
-    Navigator.pushReplacement(context, MaterialPageRoute<void>(
-        builder: (BuildContext context) => MyHomePage()));
   }
 }
